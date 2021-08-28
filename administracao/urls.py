d37c5fb35a5ffada51efa790d812b9e94010f26d
Claudio Ricardo Streppel <streppel@gmail.com>
2021-08-28 07:37:10 -0300

    Persistindo dados no Banco de Dados - concluida. Proxima: Listando todos os serviços

---
+++ administracao/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import cadastrar_servico
+
+urlpatterns =[
+    path('servicos/cadastrar', cadastrar_servico, name='cadastrar-servico'),
+]
---
+++ temp/administracao/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .forms import ServicoForm
+# Create your views here.
+
+def cadastrar_servico(request):
+    if request.method == "POST":
+        form_servico = ServicoForm(request.POST)
+        if form_servico.is_valid():
+            form_servico.save()
+    else:
+        form_servico = ServicoForm()
+    return render(request, 'servicos/form_servicos.html', {"form_servico": form_servico})
+
+
