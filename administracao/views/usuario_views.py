ce5de50aa27c4f9f95ccc3f6ffdd14cdf2c61b1b
Claudio Ricardo Streppel <streppel@gmail.com>
2021-08-30 10:21:00 -0300

    cadastro de ususarios parte 2 finalizado e funcionando

--- administracao/views/usuario_views.py
+++ administracao/views/usuario_views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from ..forms.usuario_forms import UsuarioForm
 from django.shortcuts import render
 
 def cadastrar_usuario(request):
     if request.method == "POST":
-        form_usuario = UserCreationForm(request.POST)
+        form_usuario = UsuarioForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
     else:
-        form_usuario = UserCreationForm()
+        form_usuario = UsuarioForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario':form_usuario})
