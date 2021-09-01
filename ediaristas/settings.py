357f1ec04c5813c1281c9040a6f4b18e49b82673
Claudio Ricardo Streppel <streppel@gmail.com>
2021-09-01 14:37:23 -0300

    Ativando e personalizando a pagina de resetar senha

--- ediaristas/settings.py
+++ ediaristas/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'listar_servicos'
 
+LOGOUT_REDIRECT_URL = 'logar_usuario'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
