454604f62251e47515640e5c8cbc96a398f147ec
Claudio Ricardo Streppel <streppel@gmail.com>
2021-09-01 17:05:34 -0300

    Protegendo rotas para usuarios logados. Modulo de autenticaçao finalizado

--- administracao/views/servico_views.py
+++ administracao/views/servico_views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import redirect, render
 from ..forms.servico_forms import ServicoForm
 from ..models import Servico
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+@login_required
 def cadastrar_servico(request):
     if request.method == "POST":
         form_servico = ServicoForm(request.POST)
@@ -14,11 +17,12 @@ def cadastrar_servico(request):
     return render(request, 'servicos/form_servicos.html', {"form_servico": form_servico})
 
 # Listando todos os serviços
-
+@login_required
 def listar_servicos(request):
     servicos = Servico.objects.all()
     return render(request, 'servicos/lista_servicos.html', {'servicos': servicos})
-
+    
+@login_required
 def editar_servico(request, id):
     servico = Servico.objects.get(id=id)
     form_servico = ServicoForm(request.POST or None, instance=servico)
